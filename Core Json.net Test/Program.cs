4188033f035873ad010a6e3de6c6c7938b1d2077
Basaka Mars <sunyan20@msn.com>
2019-08-01 09:59:04 +0800

    add core Json.net test

---
+++ Core Json.net Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Core_Json.net_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestObject o = new TestObject();
+            o.ID = 99;
+            o.Money = 99.99m;
+            o.Name = "Test Object";
+            o.Update = DateTime.Now.AddDays(7);
+            o.Params.Add("P1", 1);
+            o.Params.Add("P2", "String 2");
+            o.Params.Add("P3", 101.01m);
+            o.Params.Add("P4", DateTime.Now);
+            string json = JsonConvert.SerializeObject(o);
+            Console.WriteLine(json);
+            TestObject p = JsonConvert.DeserializeObject<TestObject>(json);
+            Console.WriteLine(p.Params["P4"]);
+        }
+    }
+}
---
+++ Core Json.net Test/TestObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core_Json.net_Test
+{
+    public class TestObject
+    {
+        public TestObject()
+        {
+            Params = new Dictionary<string, object>();
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Money { get; set; }
+        public DateTime Update { get; set; }
+        public Dictionary<string,object> Params { get; set; }
+    }
+}
