6870aa992eba93ef7d264fcae8f0bcdd8945b643
Basaka Mars <sunyan20@msn.com>
2019-08-13 16:11:36 +0800

    Add Customer Port Test

---
+++ Core Customer Port Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Core_Customer_Port_Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                          .AddJsonFile("host.json")
+                                          .Build();
+            return WebHost.CreateDefaultBuilder(args).UseConfiguration(configuration)
+                                                     .UseStartup<Startup>();
+        }
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
+    }
+}
